2febce2785efc2b7fc8357e9b92347f189f510c6
praveen <praveen369soni@gmail.com>
2024-12-21 19:08:42 +0530

    added 3d cube

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import Cube from "@/components/3dCube";
 import About from "@/components/About";
 import Contact from "@/components/Contact";
 import Header from "@/components/Header";
@@ -25,6 +26,27 @@ type Props = {
   socials: Social[];
 };
 
+const getRandomPosition = () => {
+  if (typeof window !== "undefined") {
+    const top = Math.floor(Math.random() * window.innerHeight);
+    const left = Math.floor(Math.random() * window.innerWidth);
+    return { top: `${top}px`, left: `${left}px` };
+  }
+  return { top: "0px", left: "0px" };
+};
+
+const getRandomSize = () => {
+  return Math.floor(Math.random() * (60 - 15 + 1)) + 15; // Random size between 15 and 60
+};
+
+const cubes = [
+{"size":50,"position":{"top":"-100px","right":"100px"},"backgroundColor":"rgb(86, 48, 61)"},
+{"size":40,"position":{"top":"-200px","right":"200px"},"backgroundColor":"rgb(96, 58, 71)"},
+{"size":36,"position":{"top":"-300px","right":"300px"},"backgroundColor":"rgb(96, 58, 71)"},
+{"size":28,"position":{"top":"-400px","right":"400px"},"backgroundColor":"rgb(96, 58, 71)"},
+{"size":18,"position":{"top":"-500px","right":"500px"},"backgroundColor":"rgb(96, 58, 71)"},
+]
+
 export default function home({
   pageInfo,
   projects,
@@ -32,7 +54,9 @@ export default function home({
   socials,
   experiences,
 }: Props) {
+  
   return (
+    
     <div
       className="bg-[rgb(36,36,40)] text-white h-screen snap-y snap-mandatory
      overflow-scroll overflow-x-hidden z-0 
@@ -80,8 +104,20 @@ export default function home({
               src={urlFor(pageInfo?.scrollUp).url()}
             />
           </div>
+     
+      
+     {cubes.map((cube, index) => (
+        <Cube
+          key={index}
+          size={cube.size}
+          position={cube.position}
+          backgroundColor={cube.backgroundColor}
+        />
+      ))}
         </footer>
       </Link>
+     
+
     </div>
   );
 }
